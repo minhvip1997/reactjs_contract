5343857468da15b7307501cb9aeaee8b67e43e80
minh <tnminh1997123@gmail.com>
2022-01-21 16:05:26 +0700

    third commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import HtmlEditor, { Toolbar, Item } from 'devextreme-react/html-editor';
 import ButtonGroup, { Item as ButtonItem } from 'devextreme-react/button-group';
@@ -6,6 +6,8 @@ import { markup } from './data.js';
 import 'devextreme/ui/html_editor/converters/markdown';
 import { useState } from 'react/cjs/react.development';
 import Header from './component/header/header.js';
+
+import { useRef } from "react";
 
 const sizeValues = ['8pt', '10pt', '12pt', '14pt', '18pt', '24pt', '36pt'];
 const fontValues = ['Arial', 'Courier New', 'Georgia', 'Impact', 'Lucida Console', 'Tahoma', 'Times New Roman', 'Verdana'];
@@ -16,6 +18,22 @@ const App =()=>{
 
   const [valueContent, setValueContent] = useState(markup())
   const [editorValueType,setEditorValueType] = useState('html')
+  const htmlEditor = useRef(null);
+  const [data, setData] = useState([]);
+    const [attribute, setAttribute] = useState([]);
+    useEffect(()=>{
+        const loaddata=()=>{
+            setData(listattr);
+        }
+        loaddata();
+        // console.log(data)
+    },[])
+
+    const listattr = [
+        {id: "1", name: "ho_va_ten", thuoctinh_id: "1"},
+        {id: "2", name: "email", thuoctinh_id: "1"},
+        {id: "3", name: "password", thuoctinh_id: "1"},
+    ]
 
  const  valueChanged=(e)=> {
 
@@ -26,12 +44,21 @@ const App =()=>{
 
     setEditorValueType(e.value)
   }
-
+  const insertText=(item)=>{
+    htmlEditor.current.instance.insertText(
+      htmlEditor.current.instance.getSelection().index,
+      item,
+      {
+        bold: true,
+        color: "green",
+      }
+    );
+  }
 
     return (
       <div className="widget-container">
-      <Header/>
-        <HtmlEditor
+      <Header htmlEditor={htmlEditor}/>
+        <HtmlEditor ref={htmlEditor}
           height={300}
           value={valueContent}
           valueType={editorValueType}
@@ -63,8 +90,14 @@ const App =()=>{
             <Item name="separator" />
             <Item name="color" />
             <Item name="background" />
+            {/* <Item>{data.map((item,index)=>{
+              return(<li key={item.id} onClick={()=>insertText(item.name)}>{item.name}</li>);
+            })}</Item> */}
           </Toolbar>
+          
         </HtmlEditor>
+
+        
 
         <div className="options">
           <ButtonGroup
@@ -82,7 +115,7 @@ const App =()=>{
     );
   
 
-  
+    
 
   
 }
--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,11 +1,19 @@
+import { HtmlEditor } from 'devextreme-react';
 import React, { useEffect } from 'react';
 import { useState } from 'react/cjs/react.development';
 import './header.css';
+import { useRef } from "react";
+
 
 function Header(props) {
 
     const [data, setData] = useState([]);
     const [attribute, setAttribute] = useState([]);
+    const htmlEditor = useRef(null);
+    const [inputName, setInputName] = useState('');
+    const [inputEmail, setInputEmail] = useState('');
+    const [inputPassword, setInputPassword] = useState('');
+    // console.log(props)
     useEffect(()=>{
         const loaddata=()=>{
             setData(listattr);
@@ -19,13 +27,46 @@ function Header(props) {
         {id: "3", name: "password", thuoctinh_id: "1"},
     ]
 
+    const insertTextAtTheBeginning = (item) => {
+        // Inserts bold, green text at the beginning of the content
+        console.log(props.htmlEditor.current.instance.getSelection(), "item");
+        props.htmlEditor.current.instance.insertText(
+            props.htmlEditor.current.instance.getSelection().index,
+          '<<'+item+'>>',
+          {
+            bold: true,
+            color: "green",
+          }
+        );
+      };
+
+      const handleInputName=(e)=>{
+        console.log(e.target.value)
+        setInputName(e.target.value)
+      }
+
+      const handleInputEmail=(e)=>{
+        console.log(e.target.value)
+        setInputEmail(e.target.value)
+      }
+
+      const handleInputPassword=(e)=>{
+        console.log(e.target.value)
+        setInputPassword(e.target.value)
+      }
 
 
     return (
         <div className="header">
             {data.length>0 && data.map((item,index)=>{
                 return(
-                    <li style={{style: "bold"}} key={item.id}>{item.name}</li>
+                    
+                    <li style={{style: "bold"}} key={item.id}>
+                    {item.name === 'ho_va_ten' ? <><input key={item.id} type="text" value={inputName} onChange={(e) => handleInputName(e)} /><button onClick={() => insertTextAtTheBeginning(item.name)}>{item.name}</button></>
+                     : item.name === 'email'? <><input key={item.id} type="text" value={inputEmail} onChange={(e) => handleInputEmail(e)} /><button onClick={() => insertTextAtTheBeginning(item.name)}>{item.name}</button></>
+                      : <><input key={item.id} type="text" value={inputPassword} onChange={(e) => handleInputPassword(e)} /><button onClick={() => insertTextAtTheBeginning(item.name)}>{item.name}</button></>}
+                    
+                    </li>
                 )
             })}
         </div>
